97e677e0eaaba70059d8c203e30a29c1fd904437
Keletso M <u14062552@tuks.co.za>
2021-01-23 10:04:51 +0200

    Data science  portfolio

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,3 +3,7 @@ from .models import Project
 # Create your views here.
 def home(request):
     return render(request, 'portfolio/index.html')
+
+def data_science(request):
+    projects = Project.objects.all()
+    return render(request, 'portfolio/data.html', {'projects': projects})
